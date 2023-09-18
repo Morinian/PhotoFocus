fdfdf99d652aab6facd5edc0ef00252ed9069d70
leckliss <erickcaique150@hotmail.com>
2023-09-18 03:34:38 -0300

    Efeito Scrool no index

--- js/js.js
+++ js/js.js
@@ -1,3 +1,17 @@
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element) )
+
+
 //Menu responsivo
 
 function menuShow() {
